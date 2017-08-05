9b14b0a5d14c1f07f62bea711d8276a13ed256ce
donkrazy <donkrazy12@gmail.com>
2017-08-05 15:57:03 +0900

    Add solutions

---
+++ Solution3.java
@@ -0,0 +1,6 @@
+class Solution {
+  public String solution(String sentence) {
+      String answer = "";
+      return answer;
+  }
+}
---
+++ Solution6.java
@@ -0,0 +1,6 @@
+class Solution {
+  public int solution(int n1, int[][] g1, int n2, int[][] g2) {
+      int answer = 0;
+      return answer;
+  }
+}
